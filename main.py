7a15ebe7ccd036b7570fea4b724b8f401841720e
Narsil92 <pblus23@gmail.com>
2024-11-27 14:35:54 +0100

    added function to count words inside frankenstein.txt also change code order of opearion so main is executed last so all other needed function can be defined for it

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ def read_book():
     with open("books/frankenstein.txt") as f:
      return f.read()
     
+
+#function to count number of words inside fankenstein.txt
+def number_of_words():
+    words=read_book()
+    output_number=words.split()
+    return(len(output_number))    
+    
 # keep file content in variable
 def main():
  book_contents = read_book()   
  print(book_contents)
+ word_count= number_of_words() #addition to get info about number of words inside frankenstein.txt
+ print(f"number of words inside frankenstein.txt is: {word_count}") #returning info about words number to the terminal
 
 # call only in this file
 if __name__ == "__main__":
